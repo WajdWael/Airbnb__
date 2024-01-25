a99ee887484395a1e7f99c641c9b1f32db0c320c
Wajd Wael <wajdabdalhay@gmail.com>
2024-01-25 08:48:04 +0300

    update images v3

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,8 @@
+import crad1image from '/public/swimming.webp'
+import crad2image from '/public/breakfast.jpg'
+import crad3image from '/public/mountain-bike.png'
+
+
 const data: {
     id: number,
     title: string,
@@ -17,7 +22,7 @@ const data: {
         title: "Swimming Life Lessons",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        image: "swimming.webp",
+        image: `${crad1image}`,
         alt: "Picture of a swimmer after she won the competition.",
         stats: {
             rating: 5.0,
@@ -31,7 +36,7 @@ const data: {
         title: "Why breakfast is so usefull?",
         description: "breakfast, food.",
         price: 125,
-        image: "breakfast.jpg",
+        image: `${crad2image}`,
         alt: "Picture of a bride wearing a white dress in a celebration hall.",
         stats: {
             rating: 5.0,
@@ -45,7 +50,7 @@ const data: {
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        image: "mountain-bike.png",
+        image: `${crad3image}`,
         alt: "Picture of a bike in the mountains.",
         stats: {
             rating: 4.8,
